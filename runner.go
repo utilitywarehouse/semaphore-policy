f62d4b5013d7b04c2cb7bf585a9058195dc3c4c2
Foivos Filippopoulos <foivosfilip@gmail.com>
2021-03-25 10:06:42 +0000

    Remove commented import

--- runner.go
+++ runner.go
@@ -6,7 +6,6 @@ import (
 
 	calicoClient "github.com/projectcalico/libcalico-go/lib/clientv3"
 	v1 "k8s.io/api/core/v1"
-	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
